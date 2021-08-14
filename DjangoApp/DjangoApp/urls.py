c23111be33080aa07a8e7e939c7aa1f11f853bab
Renato Oliveira <renato.printf@gmail.com>
2021-08-14 14:44:45 -0300

    newyear app

--- DjangoApp/DjangoApp/urls.py
+++ DjangoApp/DjangoApp/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include('hello.urls'))
+    path('hello/', include('hello.urls')),
+    path('newyear/', include('newyear.urls'))
 ]
