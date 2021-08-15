0a4b1322af381c887bef745e2ff544242d37c057
Renato Oliveira <renato.printf@gmail.com>
2021-08-15 13:36:49 -0300

    working on tasks App

--- DjangoApp/tasks/views.py
+++ DjangoApp/tasks/views.py
@@ -1,6 +1,11 @@
+from django import forms
 from django.shortcuts import render
 
 tasks = ['daa','dii','duu']
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label='New Task:')
+    priority = forms.IntegerField(label='Priority:', min_value=1,max_value=5)
 
 # Create your views here.
 def index(request):
@@ -9,4 +14,17 @@ def index(request):
     })
 
 def add(request):
-    return render(request, 'tasks/add.html')
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.changed_data('task')
+            tasks.append(task)
+        else:
+            return render(request, 'tasks/add.html', {
+        'tasks':form
+    })
+
+            
+    return render(request, 'tasks/add.html', {
+        'tasks':NewTaskForm()
+    })
