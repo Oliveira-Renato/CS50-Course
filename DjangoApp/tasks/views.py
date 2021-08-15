3bc2091c042e8fb8b4c1deec44ff959d1b794745
Renato Oliveira <renato.printf@gmail.com>
2021-08-15 13:54:08 -0300

    did some changes in the method add

--- DjangoApp/tasks/views.py
+++ DjangoApp/tasks/views.py
@@ -1,11 +1,12 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django import forms
 from django.shortcuts import render
 
-tasks = ['daa','dii','duu']
+tasks = []
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label='New Task:')
-    priority = forms.IntegerField(label='Priority:', min_value=1,max_value=5)
 
 # Create your views here.
 def index(request):
@@ -17,12 +18,13 @@ def add(request):
     if request.method == 'POST':
         form = NewTaskForm(request.POST)
         if form.is_valid():
-            task = form.changed_data('task')
+            task = form.cleaned_data['task']
             tasks.append(task)
+            return HttpResponseRedirect(reverse('tasks:index'))
         else:
             return render(request, 'tasks/add.html', {
-        'tasks':form
-    })
+                'form':form
+                })
 
             
     return render(request, 'tasks/add.html', {
