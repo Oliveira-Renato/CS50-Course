b393879b5408833d5ee5d8883c1c3cca9817fc58
Renato Oliveira <renato.printf@gmail.com>
2021-11-02 19:06:07 -0300

    Creating relations with the tables

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
 class Flight(models.Model):
-  origin = models.CharField(max_length=64)
-  destination = models.CharField(max_length=64)
-  duration = models.IntegerField()
+    origin = models.ForeignKey(Airport, on_delete=CASCADE)
+    destination = models.CharField(max_length=64)
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
