250d84accc6a41f9c825c8e028e3aedd3019d155
Renato Oliveira <renato.printf@gmail.com>
2021-11-14 16:48:24 -0300

    auth in django in progress

--- airline/users/urls.py
+++ airline/users/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
   path("", views.index, name="index"),
   path("login", views.login_view, name="login"),
-  path("logout", views.logout_view, name="logout"),
+  path("logout", views.logout_view, name="logout")
 ]
--- airline/users/views.py
+++ airline/users/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render
 # Create your views here.
+def index(request):
+  if not request.user.is_authenticated:
+    username = request.POST["username"]
+    password = request.POST["password"]
+    
+    return HttpResponseRedirect(reverse("login"))
+
 def login_view(request):
-  return None
+  return render(request, "users/login.html")
 
 def logout_view(request):
-  return None
+  pass
