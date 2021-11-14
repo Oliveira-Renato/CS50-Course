eea28eecd98fd7d4d600fe9a894bb47f2db58739
Renato Oliveira <renato.printf@gmail.com>
2021-11-14 17:11:05 -0300

    auth in django, finished

--- airline/users/views.py
+++ airline/users/views.py
@@ -6,13 +6,25 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   if not request.user.is_authenticated:
-    username = request.POST["username"]
-    password = request.POST["password"]
-    
     return HttpResponseRedirect(reverse("login"))
+  return render(request, "users/user.html")
 
 def login_view(request):
+  if request.method == "POST":
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username = username, password = password)
+    if user is not None:
+      login(request, user)
+      return HttpResponseRedirect(reverse("index"))
+    else:
+      return render(request,"users/login.html", {
+        "message": "Invalid credentials."
+      })
   return render(request, "users/login.html")
 
 def logout_view(request):
-  pass
+  logout(request)
+  return render(request, "users/login.html", {
+    "message": "Logged out."
+  })
