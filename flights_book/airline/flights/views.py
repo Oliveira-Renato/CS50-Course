439489a229488c3b5b169a0fec75c49046ba1e24
oliveira-renato <renato.printf@gmail.com>
2022-06-13 23:14:17 -0300

    feat: function to interact with flight

--- flights_book/airline/flights/views.py
+++ flights_book/airline/flights/views.py
@@ -7,3 +7,10 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request,"flights/flight.html",{
+        "flight":flight
+    })
