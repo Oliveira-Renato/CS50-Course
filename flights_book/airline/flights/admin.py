62c7f1f88369b5121f6ee2499e589dc9e4e12815
oliveira-renato <renato.printf@gmail.com>
2022-06-16 20:28:20 -0300

    perf: admin page changes

--- flights_book/airline/flights/admin.py
+++ flights_book/airline/flights/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 
 from .models import Flight, Airport, Passenger
 # Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id","origin","destination","duration")
+
 admin.site.register(Airport)
-admin.site.register(Flight)
+admin.site.register(Flight, FlightAdmin)
 admin.site.register(Passenger)
 
 
