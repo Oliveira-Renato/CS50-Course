fb463d7599bfb2023ada241cb7956f518478c0bd
oliveira-renato <renato.printf@gmail.com>
2022-06-16 20:10:50 -0300

    perf: new variable in function flight

--- flights_book/airline/flights/views.py
+++ flights_book/airline/flights/views.py
@@ -15,7 +15,8 @@ def flight(request, flight_id):
     flight = Flight.objects.get(pk=flight_id)
     return render(request,"flights/flight.html",{
         "flight":flight,
-        "passengers": flight.passengers.all()
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
 
 def book(request, flight_id):
