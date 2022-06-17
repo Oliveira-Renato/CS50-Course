b43b95762729049bb67cac26cff88f3d080a8e1f
oliveira-renato <renato.printf@gmail.com>
2022-06-16 21:21:11 -0300

    feat: authentication completed. End of this course chapter

--- flights_book/airline/users/views.py
+++ flights_book/airline/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -6,7 +7,25 @@ from django.urls import reverse
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
+    return render(request,"users/user.html")
 
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username= username, password = password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request,"users/login.html", {
+                "message": "Invalid credentials."
+            })
+
     return render(request,"users/login.html")
 
+def logout_view(request):
+    logout(request)
+    return render(request,"users/login.html", {
+        "message": "Logged out."
+    })
